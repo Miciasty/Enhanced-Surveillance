0b859d2c84efc04de9562249c213a86a0d64fca8
Miciasty <unconfigured@null.spigotmc.org>
2024-08-24 00:01:56 +0200

    Changed: - PlayerChatEvent to AsyncPlayerChatEvent
    Added:
    - Event level handling to AsyncPlayerChatEvent

--- src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/ChatEvent.java
+++ src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/ChatEvent.java
@@ -4,27 +4,73 @@ import nsk.enhanced.Managers.MonitorManager;
 import nsk.enhanced.System.ES;
 import nsk.enhanced.System.Hibernate.ChatEvent.Message;
 import nsk.enhanced.System.Hibernate.Event;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChatEvent;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ChatEvent implements Listener {
 
-    @EventHandler
-    public void onPlayerChat(PlayerChatEvent event) {
+    private static final FileConfiguration config = ES.getInstance().getBukkitEventsFile();
 
-        if (ES.getInstance().getBukkitEventsFile().getBoolean("events.PlayerChatEvent.enabled", false)) {
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+
+        if (config.getBoolean("events.AsyncPlayerChatEvent.enabled", false)) {
 
             Player player  = event.getPlayer();
 
             Map<String, String> eventData = new LinkedHashMap<>();
 
             String message = event.getMessage();
+
+            int level = config.getInt("events.AsyncPlayerChatEvent.level", 0);
+            if ( level > 0 && level < 3) {
+
+                Set<Player> recipients = event.getRecipients();
+
+                List<Double> distances = new ArrayList<>();
+                double totalDistance = 0;
+
+                for (Player recipient : recipients) {
+                    if (recipient != player) {
+
+                        Location recipientLocation = recipient.getLocation();
+                        double distance = recipientLocation.distance(player.getLocation());
+                        totalDistance += distance;
+                        distances.add(distance);
+
+                    }
+                }
+
+                eventData.put("avgDist", String.valueOf(totalDistance / (recipients.size() - 1)) );
+
+                if (level > 1) {
+
+                    double minDistance = 0;
+                    double maxDistance = 0;
+
+                    for (Double distance : distances) {
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                        }
+                        if (distance > maxDistance) {
+                            maxDistance = distance;
+                        }
+                    }
+
+                    eventData.put("minDist", String.valueOf(minDistance));
+                    eventData.put("maxDist", String.valueOf(maxDistance));
+                }
+
+            } else if (level < 0 || level > 2) {
+                ES.getInstance().getEnhancedLogger().warning("<green>'events.PlayerChatEvent.level'</green> can only be set to a maximum of 2. The provided value is invalid, so the event will default to level 0.");
+            }
 
 
             try {
