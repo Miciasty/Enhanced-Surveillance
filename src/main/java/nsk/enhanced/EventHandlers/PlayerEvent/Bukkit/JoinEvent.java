274c613090ce9ecb200614b6315734dffae6eb69
Miciasty <unconfigured@null.spigotmc.org>
2024-08-24 22:39:22 +0200

    Changed: - eventData values for each level in PlayerJoinEvent
    Added:
    - Level 1
     - "Health" value
     - "Hunger" value
    - Level 2
     - "Experience" value
     - "GameMode" value
     - "Operator" value
    - Level 3
     - "IP" value
     - "Port" value
     - "HostName" value

--- src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/JoinEvent.java
+++ src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/JoinEvent.java
@@ -3,9 +3,11 @@ package nsk.enhanced.EventHandlers.PlayerEvent.Bukkit;
 import nsk.enhanced.Managers.MonitorManager;
 import nsk.enhanced.System.ES;
 import nsk.enhanced.System.Hibernate.Event;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.LinkedHashMap;
@@ -13,15 +15,37 @@ import java.util.Map;
 
 public class JoinEvent implements Listener {
 
+    private static final FileConfiguration config = ES.getInstance().getBukkitEventsFile();
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
 
-        if (ES.getInstance().getBukkitEventsFile().getBoolean("events.PlayerJoinQuitEvent.enabled", false)) {
+        if (config.getBoolean("events.PlayerJoinQuitEvent.enabled", false)) {
 
             Player player = event.getPlayer();
 
             Map<String, String> eventData = new LinkedHashMap<>();
-            eventData.put("ip", event.getPlayer().getAddress().getAddress().getHostAddress());
+
+            int level = config.getInt("events.PlayerJoinQuitEvent.level", 0);
+            if (level > 0 && level < 4) {
+
+                eventData.put("health",     String.valueOf(player.getHealth())      .toUpperCase());
+                eventData.put("hunger",     String.valueOf(player.getFoodLevel())   .toUpperCase());
+
+                if (level > 1) {
+                    eventData.put("exp",    String.valueOf(player.getExp())         .toUpperCase());
+                    eventData.put("mode",   String.valueOf(player.getGameMode())    .toUpperCase());
+                    eventData.put("op",     String.valueOf(player.isOp())           .toUpperCase());
+                }
+
+                if (level > 2) {
+                    eventData.put("ip",     player.getAddress().getAddress().getHostAddress()     );
+                    eventData.put("port",   String.valueOf(player.getAddress().getPort())         );
+                    eventData.put("host",   player.getAddress().getHostName()       .toUpperCase());
+                }
+
+            }
+
 
             Event e = new Event("join", player, player.getLocation(), eventData);
 
