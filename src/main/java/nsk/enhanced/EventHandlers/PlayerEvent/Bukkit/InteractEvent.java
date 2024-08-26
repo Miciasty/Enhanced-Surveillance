0d494bf121d382abefff39d3e6db4cb4930d167d
Miciasty <unconfigured@null.spigotmc.org>
2024-08-26 18:37:29 +0200

    recode: added early-return "InteractEvent"

--- src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/InteractEvent.java
+++ src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/InteractEvent.java
@@ -27,122 +27,123 @@ public class InteractEvent implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
 
         if (config.getBoolean("events.PlayerInteractEvent.enabled", false)) {
+            return;
+        }
 
-            Player player = event.getPlayer();
-            Block block = event.getClickedBlock();
-            Action action = event.getAction();
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        Action action = event.getAction();
 
-            Location location;
+        Map<String, String> eventData = new LinkedHashMap<>();
 
-            Map<String, String> eventData = new LinkedHashMap<>();
+        int level = config.getInt("events.PlayerInteractEvent.level", 0);
+        if ( level > 0 && level < 4) {
 
-            int level = config.getInt("events.PlayerInteractEvent.level", 0);
-            if ( level > 0 && level < 4) {
+            if ( event.getHand() == EquipmentSlot.HAND ) {
+                eventData.put("action",             action.name().toUpperCase() );
+                if (ES.debugMode()) ES.log().info("Action: <gold>" + action.name().toUpperCase());
 
-                if ( event.getHand() == EquipmentSlot.HAND ) {
-                    eventData.put("action",             action.name().toUpperCase() );
-                    if (ES.debugMode()) ES.log().info("Action: <gold>" + action.name().toUpperCase());
+                if (level > 1) {
+                    if (event.getItem() != null && !event.getItem().getType().equals(Material.AIR)) {
+                        eventData.put("item",            event.getItem().toString().toUpperCase() );
 
-                    if (level > 1) {
-                        if (event.getItem() != null && !event.getItem().getType().equals(Material.AIR)) {
-                            eventData.put("item",            event.getItem().toString().toUpperCase() );
+                        if (ES.debugMode()) ES.log().info("Item: <gold>" + event.getItem().toString().toUpperCase());
+                    }
+                }
 
-                            if (ES.debugMode()) ES.log().info("Item: <gold>" + event.getItem().toString().toUpperCase());
+            } else if ( event.getHand() == EquipmentSlot.OFF_HAND ) {
+                eventData.put("action",             action.name().toUpperCase() );
+                if (ES.debugMode()) ES.log().info("Offhand Action: <gold>" + action.name().toUpperCase());
+
+                if (level > 1) {
+                    if (event.getItem() != null && !event.getItem().getType().equals(Material.AIR)) {
+                        eventData.put("item",            event.getItem().toString().toUpperCase() );
+
+                        if (ES.debugMode()) ES.log().info("Item: <gold>" + event.getItem().toString().toUpperCase());
+                    }
+                }
+            } else {
+                return;
+            }
+
+            if (level > 1) {
+
+                if (block != null) {
+                    eventData.put("event_block",     block.getType().toString().toUpperCase() );
+
+                    if (ES.debugMode()) ES.log().info("Block: <gold>" + block.getType().toString().toUpperCase());
+                }
+            }
+
+            if (level > 2) {
+                if (block != null) {
+
+                    eventData.put("event_face",     event.getBlockFace().toString().toUpperCase() );
+
+                    if (ES.debugMode()) ES.log().info("BlockFace: <gold>" + event.getBlockFace().toString().toUpperCase());
+
+                    boolean rdst_nrb = false;
+
+                    for (int dx = -2; dx <= 2; dx++) {
+                        for (int dz = -2; dz <= 2; dz++) {
+                            Block relativeBlock = block.getRelative(dx, 0, dz);
+                            Material type = relativeBlock.getType();
+
+                            if (type == Material.REDSTONE_WIRE || type == Material.REPEATER || type == Material.REDSTONE_TORCH || type == Material.REDSTONE_BLOCK) {
+                                rdst_nrb = true;
+                                break;
+                            }
                         }
+                        if (rdst_nrb) break;
                     }
 
-                } else if ( event.getHand() == EquipmentSlot.OFF_HAND ) {
-                    eventData.put("action",             action.name().toUpperCase() );
-                    if (ES.debugMode()) ES.log().info("Offhand Action: <gold>" + action.name().toUpperCase());
+                    eventData.put("rdst_nrb",       String.valueOf(rdst_nrb).toUpperCase());
 
-                    if (level > 1) {
-                        if (event.getItem() != null && !event.getItem().getType().equals(Material.AIR)) {
-                            eventData.put("item",            event.getItem().toString().toUpperCase() );
+                    if (ES.debugMode()) ES.log().info("Redstone nearby: <gold>" + rdst_nrb);
+                }
+            }
 
-                            if (ES.debugMode()) ES.log().info("Item: <gold>" + event.getItem().toString().toUpperCase());
-                        }
-                    }
-                } else {
+        } else if (level < 0 || level > 3) {
+            ES.getInstance().getEnhancedLogger().warning("<green>'events.PlayerChatEvent.level'</green> can only be set to a maximum of 2. The provided value is invalid, so the event will default to level 0.");
+        }
+
+        Location location;
+
+        if (block != null) {
+            location = block.getLocation();
+        } else {
+            location = null;
+        }
+
+        try {
+
+            Event lastEvent = Event.getLastEventByType(Character.getCharacter(player), "interact");
+
+            if (lastEvent != null) {
+
+                Map<String, String> lastEventData = lastEvent.getDecompressedEventData();
+
+                if (lastEventData != null && "LEFT_CLICK_BLOCK".equals(lastEventData.get("action")) && "LEFT_CLICK_AIR".equals(action.name()) ) {
                     return;
                 }
 
-                if (level > 1) {
-
-                    if (block != null) {
-                        eventData.put("event_block",     block.getType().toString().toUpperCase() );
-
-                        if (ES.debugMode()) ES.log().info("Block: <gold>" + block.getType().toString().toUpperCase());
-                    }
+                if (lastEventData != null && "RIGHT_CLICK_BLOCK".equals(lastEventData.get("action")) && "RIGHT_CLICK_AIR".equals(action.name()) ) {
+                    return;
                 }
-
-                if (level > 2) {
-                    if (block != null) {
-
-                        eventData.put("event_face",     event.getBlockFace().toString().toUpperCase() );
-
-                        if (ES.debugMode()) ES.log().info("BlockFace: <gold>" + event.getBlockFace().toString().toUpperCase());
-
-                        boolean rdst_nrb = false;
-
-                        for (int dx = -2; dx <= 2; dx++) {
-                            for (int dz = -2; dz <= 2; dz++) {
-                                Block relativeBlock = block.getRelative(dx, 0, dz);
-                                Material type = relativeBlock.getType();
-
-                                if (type == Material.REDSTONE_WIRE || type == Material.REPEATER || type == Material.REDSTONE_TORCH || type == Material.REDSTONE_BLOCK) {
-                                    rdst_nrb = true;
-                                    break;
-                                }
-                            }
-                            if (rdst_nrb) break;
-                        }
-
-                        eventData.put("rdst_nrb",       String.valueOf(rdst_nrb).toUpperCase());
-
-                        if (ES.debugMode()) ES.log().info("Redstone nearby: <gold>" + rdst_nrb);
-                    }
-                }
-
-            } else if (level < 0 || level > 3) {
-                ES.getInstance().getEnhancedLogger().warning("<green>'events.PlayerChatEvent.level'</green> can only be set to a maximum of 2. The provided value is invalid, so the event will default to level 0.");
             }
 
-            if (block != null) {
-                location = block.getLocation();
+            Event e;
+
+            if (location != null) {
+                e = new Event("interact", player, location, eventData);
             } else {
-                location = null;
+                e = new Event("interact", player, eventData);
             }
 
-            try {
+            MonitorManager.saveEvent(e);
 
-                Event lastEvent = Event.getLastEventByType(Character.getCharacter(player), "interact");
-
-                if (lastEvent != null) {
-
-                    Map<String, String> lastEventData = lastEvent.getDecompressedEventData();
-
-                    if (lastEventData != null && "LEFT_CLICK_BLOCK".equals(lastEventData.get("action")) && "LEFT_CLICK_AIR".equals(action.name()) ) {
-                        return;
-                    }
-
-                    if (lastEventData != null && "RIGHT_CLICK_BLOCK".equals(lastEventData.get("action")) && "RIGHT_CLICK_AIR".equals(action.name()) ) {
-                        return;
-                    }
-                }
-
-                Event e;
-
-                if (location != null) {
-                    e = new Event("interact", player, location, eventData);
-                } else {
-                    e = new Event("interact", player, eventData);
-                }
-
-                MonitorManager.saveEvent(e);
-            } catch (Exception e) {
-                ES.getInstance().getEnhancedLogger().severe("Failed to save PlayerEvents/interact - " + e.getMessage());
-            }
-
+        } catch (Exception e) {
+            ES.getInstance().getEnhancedLogger().severe("Failed to save PlayerEvents/interact - " + e.getMessage());
         }
 
     }
