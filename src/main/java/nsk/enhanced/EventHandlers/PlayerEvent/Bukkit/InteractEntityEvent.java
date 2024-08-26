76d34717d38b63fd4cf14cab3c4d5fbb3db82450
Miciasty <unconfigured@null.spigotmc.org>
2024-08-26 17:58:54 +0200

    recode: added early-return "InteractEntityEvent"

--- src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/InteractEntityEvent.java
+++ src/main/java/nsk/enhanced/EventHandlers/PlayerEvent/Bukkit/InteractEntityEvent.java
@@ -21,79 +21,81 @@ public class InteractEntityEvent implements Listener {
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
 
-        if (config.getBoolean("events.PlayerInteractEntityEvent.enabled", false)) {
+        if (!config.getBoolean("events.PlayerInteractEntityEvent.enabled", false)) {
+            return;
+        }
 
-            Player player = event.getPlayer();
-            Entity target = event.getRightClicked();
+        Player player = event.getPlayer();
+        Entity target = event.getRightClicked();
 
-            if (target instanceof Player || target instanceof Monster) {
-                return;
+        if (target instanceof Player || target instanceof Monster) {
+            return;
+        }
+
+        Map<String, String> eventData = new LinkedHashMap<>();
+
+        int level = config.getInt("events.PlayerInteractEntityEvent.level", 0);
+        if (level > 0 && level < 4) {
+
+            if (target.getType().getEntityClass() != null) {
+                eventData.put("type",   target.getType().getEntityClass().getSimpleName());
+                if (ES.debugMode()) ES.log().info("type: <gold>" + target.getType().getEntityClass().getSimpleName());
             }
 
-            Map<String, String> eventData = new LinkedHashMap<>();
+            if (level > 1) {
 
-            int level = config.getInt("events.PlayerInteractEntityEvent.level", 0);
-            if (level > 0 && level < 4) {
-
-                if (target.getType().getEntityClass() != null) {
-                    eventData.put("type",   target.getType().getEntityClass().getSimpleName());
-                    if (ES.debugMode()) ES.log().info("type: <gold>" + target.getType().getEntityClass().getSimpleName());
+                if (target.getCustomName() != null) {
+                    eventData.put("customName", target.getCustomName());
+                    if (ES.debugMode()) ES.log().info("health: <red>" + target.getCustomName());
                 }
 
-                if (level > 1) {
-
-                    if (target.getCustomName() != null) {
-                        eventData.put("customName", target.getCustomName());
-                        if (ES.debugMode()) ES.log().info("health: <red>" + target.getCustomName());
-                    }
-
-                    if (target instanceof LivingEntity) {
-                        eventData.put("health",     String.valueOf( ((LivingEntity) target).getHealth()) );
-                        if (ES.debugMode()) ES.log().info("health: <red>" + ((LivingEntity) target).getHealth());
-                    }
+                if (target instanceof LivingEntity) {
+                    eventData.put("health",     String.valueOf( ((LivingEntity) target).getHealth()) );
+                    if (ES.debugMode()) ES.log().info("health: <red>" + ((LivingEntity) target).getHealth());
                 }
+            }
 
-                if (level > 2) {
+            if (level > 2) {
 
-                    if (target instanceof Villager) {
+                if (target instanceof Villager) {
 
-                        Villager villager = (Villager) target;
+                    Villager villager = (Villager) target;
 
-                        if ( villager.isAdult() ) {
-                            eventData.put("vprof",      villager.getProfession().toString().toUpperCase());
-                            eventData.put("vrep",       villager.getReputation(player.getUniqueId()).toString());
-                            eventData.put("vlvl",       String.valueOf( villager.getVillagerLevel() ));
-                            eventData.put("vexp",       String.valueOf( villager.getVillagerExperience() ));
-                            eventData.put("vrstc",      String.valueOf( villager.getRestocksToday() ));
-                            eventData.put("vwork",      String.valueOf( villager.isTrading() ));
+                    if ( villager.isAdult() ) {
+                        eventData.put("vprof",      villager.getProfession().toString().toUpperCase());
+                        eventData.put("vrep",       villager.getReputation(player.getUniqueId()).toString());
+                        eventData.put("vlvl",       String.valueOf( villager.getVillagerLevel() ));
+                        eventData.put("vexp",       String.valueOf( villager.getVillagerExperience() ));
+                        eventData.put("vrstc",      String.valueOf( villager.getRestocksToday() ));
+                        eventData.put("vwork",      String.valueOf( villager.isTrading() ));
 
-                            if (ES.debugMode()) {
-                                ES.log().info("vprof: <green>" + villager.getProfession().toString().toUpperCase());
-                                ES.log().info("vrep: <green>" + villager.getReputation(player.getUniqueId()).toString());
-                                ES.log().info("vlvl: <green>" + villager.getVillagerLevel());
-                                ES.log().info("vexp: <green>" + villager.getVillagerExperience());
-                                ES.log().info("vrstc: <green>" + villager.getRestocksToday());
-                                ES.log().info("vwork: <green>" + villager.isTrading());
+                        if (ES.debugMode()) {
+                            ES.log().info("vprof: <green>" + villager.getProfession().toString().toUpperCase());
+                            ES.log().info("vrep: <green>" + villager.getReputation(player.getUniqueId()).toString());
+                            ES.log().info("vlvl: <green>" + villager.getVillagerLevel());
+                            ES.log().info("vexp: <green>" + villager.getVillagerExperience());
+                            ES.log().info("vrstc: <green>" + villager.getRestocksToday());
+                            ES.log().info("vwork: <green>" + villager.isTrading());
 
-                            }
                         }
 
+
                     }
 
                 }
 
             }
 
+        }
 
-            try {
 
-                Event e = new Event("interactEntity", player, target.getLocation(), eventData);
+        try {
 
-                MonitorManager.saveEvent(e);
-            } catch (Exception ex) {
-                ES.getInstance().getEnhancedLogger().severe("Failed to save PlayerEvents/interactEntity - " + ex.getMessage());
-            }
+            Event e = new Event("interactEntity", player, target.getLocation(), eventData);
 
+            MonitorManager.saveEvent(e);
+        } catch (Exception ex) {
+            ES.getInstance().getEnhancedLogger().severe("Failed to save PlayerEvents/interactEntity - " + ex.getMessage());
         }
     }
 
